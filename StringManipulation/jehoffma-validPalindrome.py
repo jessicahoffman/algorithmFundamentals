1c39d4a151408f4cd95daa93af6e6b4fb096355e
Jessica Hoffman <jessica.hoffman@microsoft.com>
2021-10-19 10:58:58 -0700

    cleaning code

--- StringManipulation/jehoffma-validPalindrome.py
+++ StringManipulation/jehoffma-validPalindrome.py
@@ -4,25 +4,20 @@ class Solution(object):
         :type s: str
         :rtype: bool
         """
+
         alpha = "0123456789abcdefghijklmnopqrstuvwxyz"
         left = 0
         right = len(s)-1
-        read = 0
-        s = string.lower(s)
+        s = s.lower()
 
-        while(read < len(s)/2 and left < right):
+        while left < right:
             if s[left] not in alpha:
                 left += 1
             elif s[right] not in alpha:
                 right -= 1
             elif s[left] != s[right]:
-                # print(s[left])
-                # print(s[right])
                 return False
             else:
-                # print(s[left])
-                # print(s[right])
                 left += 1
                 right -= 1
-                read += 1
         return True
